0c32c0fd88ba2e911833f9215b2ffb6fddc31854
AncyPaul26 <apaul@ecloudcontrol.com>
2020-09-26 11:24:54 +0000

    removed

--- mariadb/mariadb-10.4/scripts/archive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir=/appz/log/archive
-if [[ ! -e $dir ]]; then
-    mkdir $dir
-elif [[ ! -d $dir ]]; then
-    echo "$dir already exists " 1>&2
-fi
-
--- mariadb/mariadb-10.4/scripts/cron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-env >> /appz/scripts/.env
-sed -i -e 's/^/export /' /appz/scripts/.env
-/usr/sbin/cron -f
